1a05c34ee007f80a52d8ae1259fef94147884c6d
hamzaargoubi <hamzaargoubi@hotmail.com>
2022-07-04 20:28:22 +0100

    cruds

---
+++ src/main/java/org/esprit/ib/repositories/TransactionRepository.java
@@ -0,0 +1,10 @@
+package org.esprit.ib.repositories;
+
+import org.esprit.ib.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
---
+++ src/main/java/org/esprit/ib/services/TransactionService.java
@@ -0,0 +1,36 @@
+package org.esprit.ib.services;
+
+import org.esprit.ib.domain.Transaction;
+import org.esprit.ib.repositories.TransactionRepository;
+import org.esprit.ib.services.interfaces.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+@Service
+public class TransactionService implements ITransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+
+    @Override
+    public void save(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+        transactionRepository.deleteById(uuid);
+    }
+
+    @Override
+    public Transaction getTransactionByUUID(UUID uuid) {
+        return transactionRepository.findById(uuid).orElse(null);
+    }
+
+    @Override
+    public List<Transaction> getTransactions() {
+        return transactionRepository.findAll();
+    }
+}
